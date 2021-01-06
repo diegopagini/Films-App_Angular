fd60c6a12324521b5e931f9331340d10f67a1075
Diego Pagini <diegopaginidev@gmail.com>
2021-01-06 08:44:23 -0300

    swiper agregado

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,15 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response-';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css'],
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
   @Input() movies: Movie[];
 
   constructor() {}
+
+  ngAfterViewInit() {
+    const mySwiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      loop: true,
+    });
+  }
 
   ngOnInit(): void {
     console.log(this.movies);
