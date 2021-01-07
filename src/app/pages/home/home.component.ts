3fd3f65226e2d346d7d49ff4b0fedd9233a854a5
Diego Pagini <diegopaginidev@gmail.com>
2021-01-07 08:34:17 -0300

    infinit scroll

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response-';
 import { PeliculasService } from 'src/app/services/peliculas.service';
 
@@ -9,6 +9,25 @@ import { PeliculasService } from 'src/app/services/peliculas.service';
 })
 export class HomeComponent implements OnInit {
   public movies: Movie[] = [];
+  public moviesSlideshow: Movie[] = [];
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const position =
+      (document.documentElement.scrollTop || document.body.scrollTop) + 1300;
+
+    const maxPosition =
+      document.documentElement.scrollHeight || document.body.scrollHeight;
+
+    // console.log('position', position, 'maxPostion', maxPosition);
+
+    if (position > maxPosition) {
+      // console.log('llamar servicio');
+      this.peliculasService.getCartelera().subscribe((resp) => {
+        this.movies.push(...resp.results);
+      });
+    }
+  }
 
   constructor(private peliculasService: PeliculasService) {}
 
@@ -16,6 +35,7 @@ export class HomeComponent implements OnInit {
     this.peliculasService.getCartelera().subscribe((resp) => {
       // console.log(resp);
       this.movies = resp.results;
+      this.moviesSlideshow = resp.results;
     });
   }
 }
--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -2,16 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CarteleraResponse } from '../interfaces/cartelera-response-';
-
+import { tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
 export class PeliculasService {
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+  private carteleraPage = 1;
+
   constructor(private http: HttpClient) {}
 
+  get params() {
+    return {
+      api_key: 'b66ac5ebbf6d1157fd7f203a0224b424',
+      language: 'es-ES',
+      page: this.carteleraPage.toString(),
+    };
+  }
+
   getCartelera(): Observable<CarteleraResponse> {
-    return this.http.get<CarteleraResponse>(
-      'https://api.themoviedb.org/3/movie/now_playing?api_key=b66ac5ebbf6d1157fd7f203a0224b424&language=es-ES&page=1'
-    );
+    return this.http
+      .get<CarteleraResponse>(`${this.baseUrl}/movie/now_playing?`, {
+        params: this.params,
+      })
+      .pipe(
+        tap(() => {
+          this.carteleraPage += 1;
+        })
+      );
   }
 }
