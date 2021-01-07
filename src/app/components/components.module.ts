f71101296061e010d1a29f7541a62bc6d4173e00
Diego Pagini <diegopaginidev@gmail.com>
2021-01-07 08:10:42 -0300

    stars agregadas y funcionando ok

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,6 +4,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-poster-grid.component';
+import { RatingModule } from 'ng-starrating';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { PeliculasPosterGridComponent } from './peliculas-poster-grid/peliculas-
     SlideshowComponent,
     PeliculasPosterGridComponent,
   ],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, RatingModule],
   exports: [NavbarComponent, SlideshowComponent, PeliculasPosterGridComponent],
 })
 export class ComponentsModule {}
